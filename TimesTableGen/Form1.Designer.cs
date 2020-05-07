2bdbc4e1b36dc0a9f67855e4bdd428d15387fc97
Nepcam <cameron.nepe@gmail.com>
2020-05-07 14:16:05 +1200

    displaying to console

--- TimesTableGen/Form1.Designer.cs
+++ TimesTableGen/Form1.Designer.cs
@@ -28,60 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBoxMinNum = new System.Windows.Forms.TextBox();
             this.textBoxMaxNum = new System.Windows.Forms.TextBox();
             this.btnDisplay = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 580);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // textBoxMinNum
             // 
-            this.textBoxMinNum.Location = new System.Drawing.Point(60, 620);
+            this.textBoxMinNum.Location = new System.Drawing.Point(154, 51);
             this.textBoxMinNum.Name = "textBoxMinNum";
             this.textBoxMinNum.Size = new System.Drawing.Size(100, 20);
             this.textBoxMinNum.TabIndex = 1;
             // 
             // textBoxMaxNum
             // 
-            this.textBoxMaxNum.Location = new System.Drawing.Point(239, 620);
+            this.textBoxMaxNum.Location = new System.Drawing.Point(333, 51);
             this.textBoxMaxNum.Name = "textBoxMaxNum";
             this.textBoxMaxNum.Size = new System.Drawing.Size(100, 20);
             this.textBoxMaxNum.TabIndex = 2;
             // 
             // btnDisplay
             // 
-            this.btnDisplay.Location = new System.Drawing.Point(389, 618);
+            this.btnDisplay.Location = new System.Drawing.Point(483, 49);
             this.btnDisplay.Name = "btnDisplay";
             this.btnDisplay.Size = new System.Drawing.Size(161, 23);
             this.btnDisplay.TabIndex = 3;
             this.btnDisplay.Text = "Display";
             this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 592);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.Size = new System.Drawing.Size(301, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Enter a min and max number";
+            this.label1.Text = "Enter a minimum and maximum number between 0-9:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(203, 623);
+            this.label2.Location = new System.Drawing.Point(297, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 5;
@@ -90,7 +82,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 623);
+            this.label3.Location = new System.Drawing.Point(121, 54);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 6;
@@ -100,25 +92,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 654);
+            this.ClientSize = new System.Drawing.Size(800, 100);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDisplay);
             this.Controls.Add(this.textBoxMaxNum);
             this.Controls.Add(this.textBoxMinNum);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBoxMinNum;
         private System.Windows.Forms.TextBox textBoxMaxNum;
         private System.Windows.Forms.Button btnDisplay;
--- TimesTableGen/Form1.cs
+++ TimesTableGen/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// NAME: Cameron Nepe
+// ID:   1262199  
+
+
 namespace TimesTableGen
 {
     public partial class Form1 : Form
@@ -15,6 +19,51 @@ namespace TimesTableGen
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Variables
+                int minValue = 0;
+                int maxValue = 9;
+                int result = 0;
+                //GET minimum value
+                minValue = int.Parse(textBoxMinNum.Text);
+                //GET maximum value
+                maxValue = int.Parse(textBoxMaxNum.Text);
+                //IF max >= min THEN
+                if (maxValue >= minValue)
+                {   //FOR each value from min value to max value
+                    for (int i = minValue; i <= maxValue; i++)
+                    {
+                        //Display header for times table
+                        Console.WriteLine(i + " Times Table");
+                        for (int j = 1; j <= 9; j++)
+                        {
+                            //Your pseudo - code goes here
+                            result = j * i;
+                            Console.WriteLine(j.ToString() + " * " + i.ToString() + " = " + result.ToString());
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+                else
+                {//DISPLAY error message
+                    Console.WriteLine("Your min value should be between 0-9");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Display error message
+                Console.WriteLine(ex.Message);
+                //Clear text boxes
+                textBoxMinNum.Clear();
+                textBoxMaxNum.Clear();
+                //Set focus
+                textBoxMinNum.Focus();
+            }
         }
     }
 }
